5f303f0a7a822cc5b97ba33b833e1af5cde59786
spartacusxxxxx <spartacusxxxxxx@gmail.com>
2017-02-12 10:39:55 +0400

    index.html

--- src3/httpsender.lua
+++ src3/httpsender.lua
@@ -19,8 +19,14 @@ function sendData()
 	conn:on("receive", function(conn, payload) print(payload) end)
 -- api.thingspeak.com
 	conn:connect(8080,'192.168.43.43')
-	conn:send("GET /update?key=apikey&field1="..AOvar.."&field2="..r.." HTTP/1.1\r\n Host: 192.168.43.43:8080\r\n")
-	conn:send("Host: 192.168.43.43\r\n")
+	
+	print(conn)
+	
+	local buf = ""
+	buf = buf.."<h1> ESP8266 Web Server</h1>"
+				
+	conn:send(buf)
+	conn:send("Host: 192.168.43.43:8080\r\n")
 	conn:send("Accept: */*\r\n")
 	conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
 	conn:send("\r\n")
@@ -32,7 +38,7 @@ function sendData()
 	
 	conn:on("disconnection", function(conn)
 		print("Disconnect...")
-		print("Sleep 120 sec down...")
+		print("Sleep 2 sec down...")
 		--node.dsleep(COUNTSLEEP)
 	end)
 	
@@ -43,3 +49,12 @@ end
 tmr.alarm(0, 2000, 1, function()
 sendData()
 end)
+
+-- локальный сервер доступен в течении 20 sec на порту 8080
+srv=net.createServer(net.TCP)
+srv:listen(80,function(conn)
+conn:send("Temperature RAMEDIA:")
+conn:on("sent",function(conn) conn:close() end)
+end)
+
+
